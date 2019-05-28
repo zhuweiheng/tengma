9daa36df728f4a7c7937b85dee778c564677a753
zhuweiheng <554055345@qq.com>
2019-05-28 17:25:06 +0800

    first

---
+++ app/portal/controller/AssstorageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\portal\controller;
+
+use cmf\controller\HomeBaseController;
+
+class AssstorageController extends HomeBaseController{
+
+	public function index(){
+		return $this->fetch();
+	}
+}
